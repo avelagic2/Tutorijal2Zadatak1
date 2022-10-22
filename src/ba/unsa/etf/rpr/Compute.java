30d76b68ac1831da8cc34bdd489070a6ce96c9dd
Abdulhadi Velagic <avelagic2@etf.unsa.ba>
2022-10-22 17:09:59 +0200

    Proba zadatka 1 s tutorijala

---
+++ src/ba/unsa/etf/rpr/Compute.java
@@ -0,0 +1,26 @@
+package ba.unsa.etf.rpr;
+
+public class Compute {
+    public double broj;
+    //Konstruktor s parametrima
+    Compute(double b) {
+        broj = b;
+    }
+
+    //Mora static da bismo ga pozvali
+    public static double Sinus(double broj) {
+        return Math.sin(broj);
+    }
+    public static double Factorial(double broj) {
+        int x = 1, fact = 1;
+        while(x <= broj) {
+            fact = fact * x;
+            x++;
+        }
+        return fact;
+    }
+
+
+
+
+}
---
+++ src/ba/unsa/etf/rpr/Main.java
@@ -0,0 +1,14 @@
+package ba.unsa.etf.rpr;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner ulaz = new Scanner(System.in);
+        System.out.println("Unesite broj ->");
+        double br = ulaz.nextDouble();
+        double sine = Compute.Sinus(br);
+        double f = Compute.Factorial(br);
+        System.out.printf("Sinus broja %f je : %g\n" ,br, sine);
+        System.out.printf("Faktorijel broja %f je : %g\n" , br, f);
+    }
+}
